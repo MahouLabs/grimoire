60e1a533eea559ce13d5dbff39c534a1105a7871
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-07-04 11:13:15 -0300

    Create basic navbar and sidebar (#5)
    * Add basic top navbar
    
    * Update links colors
    
    * Add new instructions collection
    
    * Add first iteration of the sidebar

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,3 +13,17 @@ export const componentsCollection = defineCollection({
     publishedAt: z.coerce.date(),
   }),
 });
+
+export const instructionsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    type: z.literal("base").optional().default("base"),
+    name: z.literal("Instructions").optional().default("Instructions"),
+    shortTitle: z.string().optional().default("Instructions"),
+    order: z.number().optional().default(Number.POSITIVE_INFINITY),
+    title: z.string(),
+    description: z.string(),
+    lastModifiedAt: z.coerce.date().optional(),
+    publishedAt: z.coerce.date(),
+  }),
+});
