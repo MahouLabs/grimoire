2afdd9f8151f2f81e7e460b752867ca2e9025477
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-07-05 16:49:49 -0300

    Make landing page prettier for mini launch (#7)
    * WIP blur text
    
    * Update index and react's blur text
    
    * Add better code blocks
    
    * Remove code blocks
    
    * Remove astro-expressive-code
    
    * Remove more imports
    
    * Use component on landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig, envField } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue(), mdx(), svelte()],
+  integrations: [tailwind(), react(), vue(), svelte(), mdx()],
   output: "server",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
 });
