93924cc25cd0771a938fb8c332c584268a4af9d5
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-07-15 11:52:09 -0300

    Install starlight integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,20 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig, envField } from "astro/config";
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue(), svelte(), mdx()],
+  integrations: [
+    tailwind(),
+    react(),
+    vue(),
+    svelte(),
+    starlight({
+      title: "Grimoire",
+    }),
+    mdx(),
+  ],
   output: "server",
   adapter: cloudflare({
     platformProxy: {
